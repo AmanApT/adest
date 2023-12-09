c48d6d62f412b60547f2b16d2ccf72b945591624
AmanApT <amanasstudent@gmail.com>
2023-12-09 16:59:44 +0530

    hero-section-start

--- components/landingpage/Hero.tsx
+++ components/landingpage/Hero.tsx
@@ -1,11 +1,19 @@
-import '@/components/landingpage/Hero.css'
+import "@/components/landingpage/Hero.css";
 
 const Hero = () => {
   return (
-    <section className='hero-section'>
-
+    <section className="hero-section">
+      <div className="hero-section-left">
+        <p className="hero-section-heading">
+        Get your desired format at <span className="hero-span">one click</span>
+        </p>
+        <button className="get-started-btn">GET STARTED</button>
+      </div>
+      <div className="hero-section-right">
+        Lorem ipsum dolor sit amet consectetur adipisicing dfajlfjalfjladfldllkjelit. Consequatur cum, repellendus harum quod tenetur quam.
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
