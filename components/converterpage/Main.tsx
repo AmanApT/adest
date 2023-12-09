2ae9769ba36ff271defeff0dcdac941e477ad2b5
Anupreet Srivastava <anupreet.2003@gmail.com>
2023-12-09 19:40:45 +0530

    Implemented file select

--- components/converterpage/Main.tsx
+++ components/converterpage/Main.tsx
@@ -1,7 +1,60 @@
+"use client";
+
 import "@/components/converterpage/Main.css";
+import { ChangeEvent, DragEvent, useState } from "react";
 
 const Main = () => {
-  return <div className="convert-main">Main</div>;
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleClick = () => {
+    const fileInput = document.getElementById("fileInput") as HTMLInputElement;
+    fileInput.click();
+  };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setSelectedFile(file || null);
+  };
+
+  const handleDrag = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+
+    const file = event.dataTransfer.files && event.dataTransfer.files[0];
+    setSelectedFile(file || null);
+  };
+
+  const handleDragEnter = () => {
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div
+      //   className="convert-main"
+      className={`convert-main ${isDragging ? "dragging" : ""}`}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDrag}
+      onDrop={handleDrop}
+    >
+      <div>Main</div>
+      <input
+        type="file"
+        id="fileInput"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+      <button onClick={handleClick}>Press Me</button>
+      {selectedFile && <p>Selected File: {selectedFile.name}</p>}
+    </div>
+  );
 };
 
 export default Main;
