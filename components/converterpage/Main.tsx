2cb4a87c2d0ae0f118a7e33bfe440249bc698b98
Anupreet Srivastava <anupreet.2003@gmail.com>
2023-12-09 18:41:46 +0530

    Added new route

---
+++ components/converterpage/Main.tsx
@@ -0,0 +1,7 @@
+import "@/components/converterpage/Main.css";
+
+const Main = () => {
+  return <div className="convert-main">Main</div>;
+};
+
+export default Main;
---
+++ components/converterpage/Nav.tsx
@@ -0,0 +1,12 @@
+import "@/components/converterpage/Nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="convert-nav">
+      <div>Adest</div>
+      <div>Hamburger</div>
+    </nav>
+  );
+};
+
+export default Nav;
