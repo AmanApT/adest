8f32f0b6aacd79a6a4527b94e9c1d492d4cb8e51
Anupreet Srivastava <anupreet.2003@gmail.com>
2023-12-09 17:20:45 +0530

    adding after pseudo

--- components/landingpage/Hero.tsx
+++ components/landingpage/Hero.tsx
@@ -5,12 +5,15 @@ const Hero = () => {
     <section className="hero-section">
       <div className="hero-section-left">
         <p className="hero-section-heading">
-        Get your desired format at <span className="hero-span">one click</span>
+          Get your desired format at{" "}
+          <span className="hero-span">one click</span>
         </p>
         <button className="get-started-btn">GET STARTED</button>
       </div>
       <div className="hero-section-right">
-        Lorem ipsum dolor sit amet consectetur adipisicing dfajlfjalfjladfldllkjelit. Consequatur cum, repellendus harum quod tenetur quam.
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        dfajlfjalfjladfldllkjelit. Consequatur cum, repellendus harum quod
+        tenetur quam.
       </div>
     </section>
   );
