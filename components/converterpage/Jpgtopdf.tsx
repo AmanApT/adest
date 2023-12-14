27e6c9e4d70d6a8877772e204d30311b1b41a502
Anupreet Srivastava <anupreet.2003@gmail.com>
2023-12-14 13:36:46 +0530

    improved jpgtopdf

--- components/converterpage/Jpgtopdf.tsx
+++ components/converterpage/Jpgtopdf.tsx
@@ -8,6 +8,7 @@ import jsPDF from "jspdf";
 const Jpgtopdf = () => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
   const [isDragging, setIsDragging] = useState(false);
+  const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
 
   const { title } = useParams();
 
@@ -56,8 +57,10 @@ const Jpgtopdf = () => {
     }
   };
 
-  const handleDragEnter = () => {
-    setIsDragging(true);
+  const handleDragEnter = (event: DragEvent<HTMLDivElement>) => {
+    if (event.dataTransfer.types.includes("Files")) {
+      setIsDragging(true);
+    }
   };
 
   const handleDragLeave = () => {
@@ -69,31 +72,89 @@ const Jpgtopdf = () => {
       const pdf = new jsPDF();
 
       // Define padding values
-      const paddingX = 20;
+      const paddingX = 0;
       const paddingY = 20;
 
       // Calculate new dimensions considering padding
       const pdfWidth = 210 - 2 * paddingX;
       const pdfHeight = 297 - 2 * paddingY;
 
+      // Create an array of promises for fetching image data URLs
+      const imgDataUrlPromises = selectedFiles.map(async (file) => {
+        return await convertImageToPDF(file);
+      });
+
+      // Wait for all promises to resolve
+      const imgDataUrls = await Promise.all(imgDataUrlPromises);
+
+      // Create an array of promises for loading Image objects
+      const imgLoadPromises = imgDataUrls.map(
+        (imgDataUrl) =>
+          new Promise<HTMLImageElement>((resolve) => {
+            const img = new Image();
+            img.onload = () => resolve(img);
+            img.src = imgDataUrl;
+          })
+      );
+
+      // Wait for all Image objects to load
+      const images = await Promise.all(imgLoadPromises);
+
       // Iterate through selected files and add each image to the PDF
-      for (let i = 0; i < selectedFiles.length; i++) {
-        const imgDataUrl: string = await convertImageToPDF(selectedFiles[i]);
+      for (let i = 0; i < images.length; i++) {
+        const img = images[i];
+
+        // Calculate scaled dimensions maintaining the aspect ratio
+        const aspectRatio = img.width / img.height;
+        const width = pdfWidth;
+        const height = pdfWidth / aspectRatio;
+
+        const y = (pdfHeight - height) / 2 + paddingY;
+
         if (i > 0) {
           pdf.addPage();
         }
         pdf.addImage({
-          imageData: imgDataUrl,
+          imageData: img.src,
           format: "JPEG",
           x: paddingX,
-          y: paddingY,
-          width: pdfWidth,
-          height: pdfHeight,
+          y: y,
+          width: width,
+          height: height,
         });
       }
 
       pdf.save("output.pdf");
     }
+  };
+
+  const handleDeleteFile = (index: number) => {
+    setSelectedFiles((prevSelectedFiles) => {
+      const updatedFiles = [...prevSelectedFiles];
+      updatedFiles.splice(index, 1);
+      return updatedFiles;
+    });
+  };
+
+  // Handle drag events for each page 🔽
+  const handleDragStart = (event: DragEvent<HTMLDivElement>, index: number) => {
+    event.dataTransfer.setData("text/plain", String(index));
+    setDraggedIndex(index);
+  };
+
+  const handleDragOver = (event: DragEvent<HTMLDivElement>, index: number) => {
+    event.preventDefault();
+    if (index !== draggedIndex) {
+      const updatedFiles = [...selectedFiles];
+      const draggedItem = updatedFiles.splice(draggedIndex!, 1)[0];
+      updatedFiles.splice(index, 0, draggedItem);
+      setSelectedFiles(updatedFiles);
+      setDraggedIndex(index);
+    }
+  };
+
+  const handleDragEnd = () => {
+    setDraggedIndex(null);
   };
 
   return (
@@ -116,7 +177,7 @@ const Jpgtopdf = () => {
         Press Me
       </button>
       {selectedFiles.length > 0 && (
-        <div>
+        <div className="file-box">
           {/* <ul>
             {selectedFiles.map((file, index) => (
               <li key={index}>{file.name}</li>
@@ -124,7 +185,17 @@ const Jpgtopdf = () => {
           </ul> */}
           {selectedFiles.map((file, index) => (
             <>
-              <div key={index}>{file.name}</div>
+              <div
+                key={index}
+                className="each-file"
+                draggable
+                onDragStart={(e) => handleDragStart(e, index)}
+                onDragOver={(e) => handleDragOver(e, index)}
+                onDragEnd={handleDragEnd}
+              >
+                {file.name}
+                <button onClick={() => handleDeleteFile(index)}>Delete</button>
+              </div>
               {/* <img
                 src={file.imgUrl}
                 alt="Image Preview"
