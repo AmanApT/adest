d7aebade98370817650079d617a9b0f05be0c948
AmanApT <amanasstudent@gmail.com>
2023-12-25 14:20:51 +0530

    Convert Section Card Change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import "@/components/Footer.css";
 
 const Footer = () => {
   return <div className="footer"> 
-  © Created by Aman, Contributer Anupreet</div>;
+  © A Product by CredHill</div>;
 };
 
 export default Footer;
